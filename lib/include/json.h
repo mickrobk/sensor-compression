bce1c8af639052afbd6f5021de4b4ec073eb9bdf
Rob Mickle <mickrobk@gmail.com>
2024-08-17 12:31:06 -0700

    zzz

--- lib/include/json.h
+++ lib/include/json.h
@@ -5,10 +5,25 @@ using Json = nlohmann::json;
 
 #include <base64.hpp>
 
+#include "compress.h"
 #include "correction.h"
 #include "data_frame.h"
 #include "sensor.h"
 #include "time_util.h"
+
+namespace sensor_compress {
+namespace json_internal {
+inline std::string u64_to_b64(uint64_t u) {
+  auto a = u64_to_bytes(u);
+  return base64::encode_into<std::string>(a.begin(), a.end());
+}
+
+inline tl::expected<uint64_t, std::string> b64_to_u64(const std::string& s) {
+  auto a = base64::decode_into<std::vector<uint8_t>>(s);
+  return u64_from_bytes(a);
+}
+}  // namespace json_internal
+}  // namespace sensor_compress
 
 namespace nlohmann {
 template <>
@@ -20,6 +35,18 @@ struct adl_serializer<sensor_compress::utc_time_point_t> {
   static void from_json(const Json& j, sensor_compress::utc_time_point_t& p) {
     p = sensor_compress::UTCFromMs(j.template get<uint64_t>());
   }
+
+  // To store as a string...
+  // static void to_json(Json& j, const sensor_compress::utc_time_point_t& p) {
+  //   j = sensor_compress::json_internal::u64_to_b64(sensor_compress::ToMs(p));
+  // }
+
+  // static void from_json(const Json& j, sensor_compress::utc_time_point_t& p) {
+  //   auto u64_or = sensor_compress::json_internal::b64_to_u64(j.template get<std::string>());
+  //   if (u64_or) {
+  //     p = sensor_compress::UTCFromMs(*u64_or);
+  //   }
+  // }
 };
 
 template <>
