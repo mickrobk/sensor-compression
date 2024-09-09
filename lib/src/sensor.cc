1854b08b8b01a59d26bcb1c14ab32e987e49cab5
Rob Mickle <mickrobk@gmail.com>
2024-09-08 20:49:14 -0600

    print compression ratio

--- lib/src/sensor.cc
+++ lib/src/sensor.cc
@@ -1,16 +1,26 @@
 
 #include "sensor.h"
 
+#include <fmt/format.h>
+
 #include <string>
 #include <tl/expected.hpp>
 #include <vector>
+
 namespace sensor_compress {
 
 tl::expected<void, std::string> Sensor::MaybeCompressCurrent(bool force) {
   if ((force && current_frame_.size() > 0) || current_frame_.size() >= header_.frame_size) {
-    auto compressed_current = current_frame_.Compress(header_);
+    float value_compression_ratio;
+    float time_compression_ratio;
+    auto compressed_current =
+        current_frame_.Compress(header_, &value_compression_ratio, &time_compression_ratio);
     current_frame_.Clear();
     if (!compressed_current) return tl::unexpected{compressed_current.error()};
+
+    printf(fmt::format("{} compressed with ratios: values:{:.1f}, times:{:.1f}\n", header_.name,
+                       value_compression_ratio, time_compression_ratio)
+               .c_str());
     {
       std::lock_guard lock(mutex_);
       compressed_values_.push_back(std::move(compressed_current.value()));
